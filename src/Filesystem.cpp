6d692a6c514f35101e130481c54581399e92c97c
Spooky <mr.bones738@gmail.com>
2019-12-27 22:05:02 +0000

    Build system switch complete. Minecrap now runs on Linux! Windows build may be broken, but I won't find out until I get home

--- src/Filesystem.cpp
+++ src/Filesystem.cpp
@@ -11,10 +11,11 @@ Filesystem::Filesystem()
 	#ifdef WIN
 	LPSTR buf = (LPSTR)malloc(MAX_PATH);
 	GetModuleFileNameA(nullptr, buf, MAX_PATH);
+	char* lastSlash = strrchr(buf, '\\') + 1;
 	#else
 	char* buf = (char*)malloc(PATH_MAX);
 	readlink("/proc/self/exe", buf, PATH_MAX);
-	char* lastSlash = strrchr(buf, '\\') + 1;
+	char* lastSlash = strrchr(buf, '/') + 1;
 	#endif
 	*lastSlash = 0; // null terminate early
 	exePath = std::string(buf);
